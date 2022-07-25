264dda2d09da305db3ddda66a01dedf4487346eb
FOLI <you@example.com>
2022-07-25 22:47:36 +0430

    add component skeleton

---
+++ src/components/styled-button.tsx
@@ -0,0 +1,52 @@
+import { ReactNode } from "react"
+import "./styled-button.css"
+
+type StyledButtonProps = {
+  text: string
+  icon?: ReactNode
+  iconSide?: "left" | "right"
+  disabled?: boolean
+  type?: "button" | "submit" | "reset"
+  rounded?: "normal" | "rounded"
+  variant?: "primary" | "accent" | "faded" | "warning" | "error" | "action"
+  href?: string
+}
+
+export const StyledButton = ({
+  text,
+  icon,
+  iconSide = "right",
+  disabled = false,
+  type,
+  href,
+  rounded = "normal",
+  variant = "faded",
+}: StyledButtonProps) => {
+  const content = (
+    <>
+      {icon && iconSide === "left" ? <span>{icon}</span> : null}
+      {text} {icon && iconSide === "right" ? <span>{icon}</span> : null}
+    </>
+  )
+  return href ? (
+    <a
+      href={href}
+      className="styled-button"
+      data-rounded={rounded}
+      data-disabled={disabled}
+      data-variant={variant}
+    >
+      {content}
+    </a>
+  ) : (
+    <button
+      data-rounded={rounded}
+      type={type ? type : "button"}
+      disabled={disabled}
+      className="styled-button"
+      data-variant={variant}
+    >
+      {content}
+    </button>
+  )
+}
