ee92b4a0b306199cc49309f8b3d8aacd3401ca90
FOLI <you@example.com>
2022-07-26 22:07:19 +0430

    add skeleton and prop type

---
+++ src/components/card.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from "react"
+import "./card.css"
+
+type Card = {
+    img?: ReactNode
+    title: string
+    categories: Array<string>
+    score: string
+    scoreNumber: string
+    delivery: string
+    deliveryPrice : string
+}
+export const Card = ({ }) => {
+    return (
+
+    )
+}
