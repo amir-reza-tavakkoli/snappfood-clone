10abf25437d13731a4f1abd482307a00551147c7
FOLI <you@example.com>
2022-07-26 13:58:31 +0430

    remove clutter in type props

--- src/components/styled-button.tsx
+++ src/components/styled-button.tsx
@@ -4,31 +4,29 @@ import "./styled-button.css"
 type ButtonProps = {
   text: string
   icon?: ReactNode
-  iconSide?: "left" | "right"
   disabled?: boolean
   type?: "button" | "submit" | "reset"
   rounded?: "normal" | "rounded"
   variant?: "primary" | "accent" | "faded" | "warning" | "error" | "action"
-  href?: string
 }
 
 export const Button = ({
   text,
   icon,
   disabled = false,
-  type,
+  type = "button",
   rounded = "normal",
   variant = "faded",
 }: ButtonProps) => {
   return (
     <button
       data-rounded={rounded}
-      type={type ? type : "button"}
+      type={type}
       disabled={disabled}
       className="styled-button"
       data-variant={variant}
     >
-      {text} {icon && iconSide === "right" ? <span>{icon}</span> : null}
+      {text} {icon ? <span>{icon}</span> : null}
     </button>
   )
 }
