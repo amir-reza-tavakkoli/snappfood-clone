8d41164c697fe32567756793db563febbadb4a4f
FOLI <you@example.com>
2022-07-27 10:41:11 +0430

    add basic markup for card

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,17 +1,47 @@
 import { ReactNode } from "react"
 import "./card.css"
+import { StarIcon } from "./svg"
 
-type Card = {
-    img?: ReactNode
-    title: string
-    categories: Array<string>
-    score: string
-    scoreNumber: string
-    delivery: string
-    deliveryPrice : string
+type CardProps = {
+  img?: ReactNode
+  title: string
+  categories?: Array<string>
+  score?: string
+  scoreNumber?: string
+  delivery?: string
+  deliveryPrice?: string
+  deliveryIcon?: ReactNode
+  offer?: string
+  icon?: ReactNode
 }
-export const Card = ({ }) => {
-    return (
-
-    )
+export const Card = ({
+  img,
+  icon,
+  title,
+  delivery,
+  deliveryIcon,
+  deliveryPrice,
+  categories,
+  score,
+  scoreNumber,
+  offer,
+}: CardProps) => {
+  const categoriesString = categories?.join(", ")
+  return (
+    <article className="card">
+      <div className="card-image">
+        {img} {icon} <div>{offer}</div>
+      </div>
+      <h3>{title}</h3>
+      <small>
+        <StarIcon />
+        {score}({scoreNumber}){" "}
+      </small>
+      <small>{categoriesString}</small>
+      <p className="card-delivery">
+        {deliveryIcon}
+        {delivery} {deliveryPrice}
+      </p>
+    </article>
+  )
 }
