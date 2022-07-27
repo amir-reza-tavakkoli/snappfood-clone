02e8e4bb7f7cfc5e52e64dc809a7169cdf0a4537
FOLI <you@example.com>
2022-07-27 11:56:59 +0430

    add essentail data for story

--- src/components/card.stories.tsx
+++ src/components/card.stories.tsx
@@ -1,6 +1,16 @@
 import { Card } from "./card"
-import { OrderIcon } from "./svg"
+import { DeliveryGuyIcon } from "./svg"
+import  "../assets/img.jpg"
 
 export const faded = () => (
-  <Card title="yy"></Card>
+  <Card
+    title="yy"
+    categories={[]}
+    score="4.7"
+    scoreNumber="3834"
+    delivery="فروشنده"
+    deliveryIcon = {<DeliveryGuyIcon/>}
+    deliveryPrice="7000 تومان"
+    img={<img src="../assets/img.jpg"></img>}
+  ></Card>
 )
