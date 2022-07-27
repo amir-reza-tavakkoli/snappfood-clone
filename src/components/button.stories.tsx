06731bcf5d47904819b348ead5d25d0dd763dcc5
FOLI <you@example.com>
2022-07-27 22:11:50 +0430

    organize stories

--- src/components/button.stories.tsx
+++ src/components/button.stories.tsx
@@ -1,46 +1,38 @@
 import { Button } from "./button"
-import { OrderIcon } from "./svg"
+import { OrderIcon, LeftPinkFlashIcon, UserIconFaded } from "./svg"
 
-export const defaultVariant = () => (
-  <Button></Button>
-)
+export const defaultVariant = () => <Button>پیتزا</Button>
 
-export const fadedVariant = () => (
-  <Button icon={<OrderIcon />} variant="faded">
-    button
-  </Button>
-)
+export const fadedVariantRTL = () => <Button variant="faded">انصراف</Button>
 
-export const accentVariant = () => (
+export const accentVariantIcon = () => (
   <div style={{ inlineSize: "40px" }}>
-    <Button
-      icon={<OrderIcon />}
-      variant="accent"
-      rounding="full"
-      className="fff"
-      disabled={false}
-    >
-      button
+    <Button icon={<OrderIcon />} variant="accent">
+      offers
     </Button>
   </div>
 )
-export const RTLAccentVariant = () => (
-  <div style={{ inlineSize: "40px" }}>
-    <Button
-      dir="rtl"
-      icon={<OrderIcon />}
-      variant="accent"
-      rounding="full"
-      disabled={false}
-    >
-      ممد
+
+export const accentVariantConstraintedIconRTL = () => (
+  <div style={{ inlineSize: "280px" }}>
+    <Button dir="rtl" icon={<UserIconFaded />} variant="accent">
+      ورود به حساب کاربری
     </Button>
   </div>
 )
-export const primaryVariant = () => (
+
+export const accentRuondedIconRTL = () => (
   <div style={{ inlineSize: "40px" }}>
-    <Button icon={<OrderIcon />} variant="primary">
-      button
+    <Button icon={<OrderIcon />} variant="accent" rounding="full">
+      سفارش
+    </Button>
+  </div>
+)
+
+export const fadedVariantIconConstraintedRTL = () => (
+  <div style={{ inlineSize: "40px" }}>
+    <Button icon={<UserIconFaded />} variant="faded">
+      سفارش
     </Button>
   </div>
 )
