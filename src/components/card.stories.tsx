46166d531f8e9158713660809baface05f2232f6
FOLI <you@example.com>
2022-07-28 14:09:27 +0430

    pass store icon from story to card

--- src/components/card.stories.tsx
+++ src/components/card.stories.tsx
@@ -1,8 +1,10 @@
 import { Card } from "./card"
 import { DeliveryGuyIcon } from "./svg"
 import image from "../assets/img.webp"
+import storeIcon from "../assets/store-icon.webp"
 
 const img = <img src={image}></img>
+const storeIco = <img src={storeIcon}></img>
 
 export const faded = () => (
   <Card
@@ -10,9 +12,10 @@ export const faded = () => (
     categories={["کباب", "ایرانی", "خورشت"]}
     score="۳.۶"
     scoreNumber="۱۲,۳۶۶"
+    storeIcon = {storeIco}
     delivery="پیک فروشنده"
     deliveryIcon={<DeliveryGuyIcon />}
-    deliveryPrice=" ۷,۰۰۰  تومان"
+    deliveryOption=" ۷,۰۰۰  تومان"
     img={img}
     dir="rtl"
   ></Card>
