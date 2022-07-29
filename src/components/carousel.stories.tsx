107a661781149d19a10998a598f4fa85a31852d2
FOLI <you@example.com>
2022-07-29 14:19:29 +0430

    add json data to story

--- src/components/carousel.stories.tsx
+++ src/components/carousel.stories.tsx
@@ -1,15 +1,105 @@
-import { Carousel } from "./carousel";
-// import storeIcon from "../assets/store-icon.webp";
-import type { item } from "./carousel";
+import { Carousel } from "./carousel"
+import type { item } from "./carousel"
 
-const item1 : item =
-    {name:"1رستوران" , src:"../assets/store-icon.webp" , key: 1, href:"#"}
-const item2 : item =
-    {name:"2رستوران" , src:"../assets/store-icon.webp" , key: 1, href:"#"}
-
+const item1: item = {
+  name: "رستوران",
+  src: "https://snappfood.ir/static/images/pin.svg",
+  key: 1,
+  href: "#",
+}
+const items: item[] = [
+  {
+    name: " رستوران",
+    src: "https://snappfood.ir/static/images/pin.svg",
+    key: 2,
+    href: "#",
+  },
+  {
+    name: "رستوران",
+    src: "https://snappfood.ir/static/images/pin.svg",
+    key: 3,
+    href: "#",
+  },
+  {
+    name: "رستوران",
+    src: "https://snappfood.ir/static/images/pin.svg",
+    key: 4,
+    href: "#",
+  },
+  {
+    name: "رستوران",
+    src: "https://snappfood.ir/static/images/pin.svg",
+    key: 5,
+    href: "#",
+  },
+  {
+    name: "رستوران",
+    src: "https://snappfood.ir/static/images/pin.svg",
+    key: 6,
+    href: "#",
+  },
+  {
+    name: "رستوران",
+    src: "https://snappfood.ir/static/images/pin.svg",
+    key: 7,
+    href: "#",
+  },
+  {
+    name: "رستوران",
+    src: "https://snappfood.ir/static/images/pin.svg",
+    key: 8,
+    href: "#",
+  },
+  {
+    name: "رستوران",
+    src: "https://snappfood.ir/static/images/pin.svg",
+    key: 9,
+    href: "#",
+  },
+  {
+    name: "رستوران",
+    src: "https://snappfood.ir/static/images/pin.svg",
+    key: 10,
+    href: "#",
+  },
+  {
+    name: "رستوران",
+    src: "https://snappfood.ir/static/images/pin.svg",
+    key: 11,
+    href: "#",
+  },
+  {
+    name: "رستوران",
+    src: "https://snappfood.ir/static/images/pin.svg",
+    key: 12,
+    href: "#",
+  },
+  {
+    name: "رستوران",
+    src: "https://snappfood.ir/static/images/pin.svg",
+    key: 13,
+    href: "#",
+  },
+  {
+    name: "رستوران",
+    src: "https://snappfood.ir/static/images/pin.svg",
+    key: 14,
+    href: "#",
+  },
+  {
+    name: "رستوران",
+    src: "https://snappfood.ir/static/images/pin.svg",
+    key: 15,
+    href: "#",
+  },
+  {
+    name: "رستوران",
+    src: "https://snappfood.ir/static/images/pin.svg",
+    key: 16,
+    href: "#",
+  },
+]
 
 export const carousel = () => {
-    return (
-        <Carousel items={[item1, item2]} />
-    );
+  return <Carousel items={[item1, ...items]} />
 }
