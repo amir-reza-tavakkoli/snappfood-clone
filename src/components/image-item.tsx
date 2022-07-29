fec9349f9b293bba3a8c0e9b55fcc8b42c2ed76e
FOLI <you@example.com>
2022-07-29 19:54:24 +0430

    add icon to markup

--- src/components/image-item.tsx
+++ src/components/image-item.tsx
@@ -1,16 +1,20 @@
-import React from "react"
+import type { ReactNode } from "react"
 import "./image-item.css"
+import { LeftPinkFlashIcon } from "./svg"
 
 export type ImageItemProps = {
   title: string
   src: string
+  icon?: ReactNode
 }
 
-export const ImageItem = ({ title, src }: ImageItemProps) => {
+export const ImageItem = ({ title, src, icon }: ImageItemProps) => {
   return (
     <figure className="image-item">
       <img src={src} alt={title} />
-      <figcaption>{title}</figcaption>
+      <figcaption>
+        {title} {icon ? <span role="presentation">{icon}</span> : null}
+      </figcaption>
     </figure>
   )
 }
