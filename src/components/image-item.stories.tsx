5d9a33cbb001d3e9ce0fbddbbf96db276d811649
FOLI <you@example.com>
2022-07-29 19:54:41 +0430

    make icon in story

--- src/components/image-item.stories.tsx
+++ src/components/image-item.stories.tsx
@@ -1,16 +1,15 @@
 import { ImageItem } from "./image-item"
 import type { ImageItemProps } from "./image-item"
+import { LeftPinkFlashIcon } from "./svg"
+
+const defaultIcon = <LeftPinkFlashIcon />
 
 const imageItemProps: ImageItemProps = {
   src: "https://cdn.snappfood.ir/uploads/images/tags/website_image_irani_1.jpg",
   title: "ایرانی",
+  icon: defaultIcon,
 }
 
 export const ImageItemStory = () => {
-  return (
-    <ImageItem
-      title={imageItemProps.title}
-      src={imageItemProps.src}
-    ></ImageItem>
-  )
+  return <ImageItem {...imageItemProps}></ImageItem>
 }
