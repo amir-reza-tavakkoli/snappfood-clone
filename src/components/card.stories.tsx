80ec1f21026b64ad649703e67c57e7f7920635a6
FOLI <you@example.com>
2022-07-31 16:11:02 +0430

    refactor story to use new Card component

--- src/components/card.stories.tsx
+++ src/components/card.stories.tsx
@@ -3,6 +3,7 @@ import { ComponentProps } from "react"
 
 const cardProps : ComponentProps<typeof Card> = {
   name: "رستوران بگ میرزا",
+  type: "رستوران",
   icons: {
     icon: <img src="https://cdn.snappfood.ir/media/cache/vendor_logo/uploads/images/vendors/logos/623209826af52.jpg" />,
     image: "https://cdn.snappfood.ir/300x200/cdn/vendor_sub_types/7/01.jpg",
@@ -29,6 +30,19 @@ export const card = () => {
 export const cardAndConstrainted = () => {
   return (
     <div
+      style={{
+        width: "350px",
+        height: "300px",
+      }}
+    >
+      <Card {...cardProps}></Card>
+    </div>
+  )
+}
+export const cardAndConstraintedAndRTL = () => {
+  return (
+    <div
+      dir="rtl"
       style={{
         width: "350px",
         height: "300px",
