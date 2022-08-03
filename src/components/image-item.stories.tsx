2bf3f35706c71262ef856e1fa7966b0938689af7
Amir <72388304+amir-reza-tavakkoli@users.noreply.github.com>
2022-08-03 18:56:08 +0430

    format

--- src/components/image-item.stories.tsx
+++ src/components/image-item.stories.tsx
@@ -1,15 +1,16 @@
 import { ImageItem } from "./image-item"
-import { ComponentProps } from "react";
+import { ComponentProps } from "react"
 
-const imageItemProps: ComponentProps<typeof ImageItem>  = {
+const imageItemProps: ComponentProps<typeof ImageItem> = {
   src: "https://cdn.snappfood.ir/uploads/images/tags/website_image_irani_1.jpg",
-  type : "غذا",
+  type: "غذا",
   title: "ایرانی",
 }
 
 export const ImageItemStory = () => {
   return <ImageItem {...imageItemProps}></ImageItem>
 }
+
 export const ImageItemStoryConstrained = () => {
   return (
     <div style={{ width: "500px" }}>
