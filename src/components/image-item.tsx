9fbd36cd46e72ed5bf137322444bdccaca50a20f
Amir <72388304+amir-reza-tavakkoli@users.noreply.github.com>
2022-08-03 18:45:50 +0430

    remove clutter

--- src/components/image-item.tsx
+++ src/components/image-item.tsx
@@ -1,18 +1,22 @@
-import type { ReactNode } from "react"
 import "./image-item.css"
 import { LeftPinkFlashIcon } from "./svg"
 
 export type ImageItemProps = {
   title: string
+  type?: string
   src: string
 }
 
-export const ImageItem = ({ title, src }: ImageItemProps) => {
+export const ImageItem = ({ title, src, type }: ImageItemProps) => {
   return (
     <figure className="image-item">
+      {type ? <p className="nonvisual">{type}</p> : null}
       <img src={src} alt={title} />
       <figcaption className="_title">
-        {title} <span role="presentation"> <LeftPinkFlashIcon/></span>
+        {title}{" "}
+        <span role="presentation">
+          <LeftPinkFlashIcon />
+        </span>
       </figcaption>
     </figure>
   )
