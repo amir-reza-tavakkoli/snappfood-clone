a9b44c88f91035ad94b52edeb64c021412b811f0
Amir <72388304+amir-reza-tavakkoli@users.noreply.github.com>
2022-08-03 14:27:17 +0430

    add better data for story and use component props

--- src/components/carousel.stories.tsx
+++ src/components/carousel.stories.tsx
@@ -1,100 +1,85 @@
 import { Carousel } from "./carousel"
-import type { item } from "./carousel"
 
+import type { ComponentProps } from "react"
 
-const items: item[] = [
-  {
-    name: "ستوران",
-    src: "https://snappfood.ir/static/images/pin.svg",
-    key: 2,
-    href: "#",
-  },
-  {
-    name: "رستوران",
-    src: "https://snappfood.ir/static/images/pin.svg",
-    key: 3,
-    href: "#",
-  },
-  {
-    name: "رستوران",
-    src: "https://snappfood.ir/static/images/pin.svg",
-    key: 4,
-    href: "#",
-  },
-  {
-    name: "رستوران",
-    src: "https://snappfood.ir/static/images/pin.svg",
-    key: 5,
-    href: "#",
-  },
-  {
-    name: "رستوران",
-    src: "https://snappfood.ir/static/images/pin.svg",
-    key: 6,
-    href: "#",
-  },
-  {
-    name: "رستوران",
-    src: "https://snappfood.ir/static/images/pin.svg",
-    key: 7,
-    href: "#",
-  },
-  {
-    name: "رستوران",
-    src: "https://snappfood.ir/static/images/pin.svg",
-    key: 8,
-    href: "#",
-  },
-  {
-    name: "رستوران",
-    src: "https://snappfood.ir/static/images/pin.svg",
-    key: 9,
-    href: "#",
-  },
-  {
-    name: "رستوران",
-    src: "https://snappfood.ir/static/images/pin.svg",
-    key: 10,
-    href: "#",
-  },
-  {
-    name: "رستوران",
-    src: "https://snappfood.ir/static/images/pin.svg",
-    key: 11,
-    href: "#",
-  },
-  {
-    name: "رستوران",
-    src: "https://snappfood.ir/static/images/pin.svg",
-    key: 12,
-    href: "#",
-  },
-  {
-    name: "رستوران",
-    src: "https://snappfood.ir/static/images/pin.svg",
-    key: 13,
-    href: "#",
-  },
-  {
-    name: "رستوران",
-    src: "https://snappfood.ir/static/images/pin.svg",
-    key: 14,
-    href: "#",
-  },
-  {
-    name: "رستوران",
-    src: "https://snappfood.ir/static/images/pin.svg",
-    key: 15,
-    href: "#",
-  },
-  {
-    name: "رستوران",
-    src: "https://snappfood.ir/static/images/pin.svg",
-    key: 16,
-    href: "#",
-  },
-]
+const items: ComponentProps<typeof Carousel> = {
+  type: "food",
+  items: [
+    {
+      name: "فست فود",
+      src: "https://cdn.snappfood.ir/uploads/images/review-app/icons/count/desktop_1_603508bf202d8_img_st_food.png",
+      key: 2,
+      href: "#",
+    },
+    {
+      name: "نان",
+      src: "https://cdn.snappfood.ir/uploads/images/review-app/icons/count/desktop_5_60350898c61b5_img_st_bakery.png",
+      key: 3,
+      href: "#",
+    },
+    {
+      name: "گوشت",
+      src: "https://cdn.snappfood.ir/uploads/images/review-app/icons/count/desktop_11_603507afc9a32_img_st_meat.png",
+      key: 4,
+      href: "#",
+    },
+    {
+      name: "میوه",
+      src: "https://cdn.snappfood.ir/uploads/images/review-app/icons/count/desktop_6_6035088cbcde4_img_st_fruit.png",
+      key: 5,
+      href: "#",
+    },
+    {
+      name: "آجیل",
+      src: "https://cdn.snappfood.ir/uploads/images/review-app/icons/count/desktop_7_60350883d6e43_img_st_nut.png",
+      key: 6,
+      href: "#",
+    },
+    {
+      name: "سوپری",
+      src: "https://cdn.snappfood.ir/uploads/images/review-app/icons/count/desktop_4_603508a14ab73_img_st_supermarket.png",
+      key: 7,
+      href: "#",
+    },
+    {
+      name: "فست فود",
+      src: "https://cdn.snappfood.ir/uploads/images/review-app/icons/count/desktop_1_603508bf202d8_img_st_food.png",
+      key: 12,
+      href: "#",
+    },
+    {
+      name: "نان",
+      src: "https://cdn.snappfood.ir/uploads/images/review-app/icons/count/desktop_5_60350898c61b5_img_st_bakery.png",
+      key: 13,
+      href: "#",
+    },
+    {
+      name: "گوشت",
+      src: "https://cdn.snappfood.ir/uploads/images/review-app/icons/count/desktop_11_603507afc9a32_img_st_meat.png",
+      key: 14,
+      href: "#",
+    },
+    {
+      name: "میوه",
+      src: "https://cdn.snappfood.ir/uploads/images/review-app/icons/count/desktop_6_6035088cbcde4_img_st_fruit.png",
+      key: 15,
+      href: "#",
+    },
+    {
+      name: "آجیل",
+      src: "https://cdn.snappfood.ir/uploads/images/review-app/icons/count/desktop_7_60350883d6e43_img_st_nut.png",
+      key: 16,
+      href: "#",
+    },
+    {
+      name: "سوپری",
+      src: "https://cdn.snappfood.ir/uploads/images/review-app/icons/count/desktop_4_603508a14ab73_img_st_supermarket.png",
+      key: 17,
+      href: "#",
+    },
+  ],
+}
 
 export const carousel = () => {
-  return <Carousel items={items} />
+  return <Carousel {...items} />
 }
