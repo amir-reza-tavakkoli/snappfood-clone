e5d3013dae874a6425b1c52a4c18d0b0909a56c7
Amir <72388304+amir-reza-tavakkoli@users.noreply.github.com>
2022-08-03 11:18:10 +0430

    make classes more simple

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -3,24 +3,27 @@ import "./carousel.css"
 export type item = { name: string; src: string; href: string; key: number }
 
 type CarouselProps = {
+  type: string
   items: item[]
 }
 
 export const Carousel = ({ items }: CarouselProps) => {
   return (
-    <ul className="carousel">
-      {items.map((item, index, array) => {
-        return (
-          <li key={item.key}>
-            <a href={item.href}>
-              <figure>
-                <img src={item.src} alt={item.name}></img>
-                <figcaption>{item.name}</figcaption>
-              </figure>
-            </a>
-          </li>
-        )
-      })}
-    </ul>
+    <nav>
+      <ul className="carousel">
+        {items.map((item, index, array) => {
+          return (
+            <li key={item.key}>
+              <a href={item.href} className="_link">
+                <div>
+                  <img src={item.src} alt={item.name}></img>
+                  <span className="_name">{item.name}</span>
+                </div>
+              </a>
+            </li>
+          )
+        })}
+      </ul>
+    </nav>
   )
 }
