dc327b92246436b640197fc43b8adf16ac13b8f2
Amir <72388304+amir-reza-tavakkoli@users.noreply.github.com>
2022-08-04 16:06:44 +0430

    add story for image as node

--- src/components/vendor-card.stories.tsx
+++ src/components/vendor-card.stories.tsx
@@ -36,8 +36,21 @@ export const DefaultCard = () => {
   return <VendorCard {...cardProps}></VendorCard>
 }
 
+export const WithImageNode = () => {
+  const props = {
+    ...cardProps,
+    image: (
+      <img
+        src="https://cdn.snappfood.ir/300x200/cdn/vendor_sub_types/7/03.jpg"
+        alt="cool alt text"
+      />
+    ),
+  }
+  return <VendorCard {...props}></VendorCard>
+}
+
 export const WithoutRating = () => {
-  const props = {...cardProps, rating: undefined}
+  const props = { ...cardProps, rating: undefined }
   return <VendorCard {...props}></VendorCard>
 }
 
@@ -62,12 +75,12 @@ export const WithoutRatingValue = () => {
 }
 
 export const WithoutLogo = () => {
-    const props = { ...cardProps, logo: undefined }
+  const props = { ...cardProps, logo: undefined }
   return <VendorCard {...props}></VendorCard>
 }
 
 export const WithoutTags = () => {
-    const props = { ...cardProps, tags: undefined }
+  const props = { ...cardProps, tags: undefined }
   return <VendorCard {...props}></VendorCard>
 }
 
@@ -77,51 +90,52 @@ export const WithoutDiscount = () => {
 }
 
 export const WitTagsOverflow = () => {
-    const props = {
-      ...cardProps,
-      tags: ["Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."],
-    }
+  const props = {
+    ...cardProps,
+    tags: [
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    ],
+  }
   return <VendorCard {...props}></VendorCard>
 }
 
 export const WitNameOverflow = () => {
-    const props = {
-      ...cardProps,
-      name: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    }
+  const props = {
+    ...cardProps,
+    name: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  }
   return <VendorCard {...props}></VendorCard>
 }
 
 export const WithoutDelivery = () => {
-    const props = {
-      ...cardProps,
-      delivery : undefined,
-    }
+  const props = {
+    ...cardProps,
+    delivery: undefined,
+  }
   return <VendorCard {...props}></VendorCard>
 }
 
 export const WithoutDeliveryMethod = () => {
-    const props = {
-      ...cardProps,
-      delivery: {
-        price: {
-          value : 6000
-        }
+  const props = {
+    ...cardProps,
+    delivery: {
+      price: {
+        value: 6000,
       },
-    }
+    },
+  }
   return <VendorCard {...props}></VendorCard>
 }
 
 export const WithoutDeliveryPrice = () => {
-      const props = {
-        ...cardProps,
-        delivery: {
-          method : "پیش سفارش"
-        },
-      }
+  const props = {
+    ...cardProps,
+    delivery: {
+      method: "پیش سفارش",
+    },
+  }
   return <VendorCard {...props}></VendorCard>
 }
-
 
 export const Constrained = () => {
   return (
@@ -130,7 +144,6 @@ export const Constrained = () => {
     </div>
   )
 }
-
 
 export const ConstrainedRTL = () => {
   return (
