a5d74640e94350bbe2da416c867aff29af4374e1
Amir <72388304+amir-reza-tavakkoli@users.noreply.github.com>
2022-08-04 18:18:11 +0430

    add new stories

--- src/components/vendor-nav.stories.tsx
+++ src/components/vendor-nav.stories.tsx
@@ -7,73 +7,85 @@ const props: ComponentProps<typeof VendorNav> = {
   items: [
     {
       name: "فست فود",
-      src: "https://cdn.snappfood.ir/uploads/images/review-app/icons/count/desktop_1_603508bf202d8_img_st_food.png",
+      image:
+        "https://cdn.snappfood.ir/uploads/images/review-app/icons/count/desktop_1_603508bf202d8_img_st_food.png",
       key: 2,
       href: "#",
     },
     {
       name: "نان",
-      src: "https://cdn.snappfood.ir/uploads/images/review-app/icons/count/desktop_5_60350898c61b5_img_st_bakery.png",
+      image:
+        "https://cdn.snappfood.ir/uploads/images/review-app/icons/count/desktop_5_60350898c61b5_img_st_bakery.png",
       key: 3,
       href: "#",
     },
     {
       name: "گوشت",
-      src: "https://cdn.snappfood.ir/uploads/images/review-app/icons/count/desktop_11_603507afc9a32_img_st_meat.png",
+      image:
+        "https://cdn.snappfood.ir/uploads/images/review-app/icons/count/desktop_11_603507afc9a32_img_st_meat.png",
       key: 4,
       href: "#",
     },
     {
       name: "میوه",
-      src: "https://cdn.snappfood.ir/uploads/images/review-app/icons/count/desktop_6_6035088cbcde4_img_st_fruit.png",
+      image:
+        "https://cdn.snappfood.ir/uploads/images/review-app/icons/count/desktop_6_6035088cbcde4_img_st_fruit.png",
       key: 5,
       href: "#",
     },
     {
       name: "آجیل",
-      src: "https://cdn.snappfood.ir/uploads/images/review-app/icons/count/desktop_7_60350883d6e43_img_st_nut.png",
+      image:
+        "https://cdn.snappfood.ir/uploads/images/review-app/icons/count/desktop_7_60350883d6e43_img_st_nut.png",
       key: 6,
       href: "#",
     },
     {
       name: "سوپری",
-      src: "https://cdn.snappfood.ir/uploads/images/review-app/icons/count/desktop_4_603508a14ab73_img_st_supermarket.png",
+      image:
+        "https://cdn.snappfood.ir/uploads/images/review-app/icons/count/desktop_4_603508a14ab73_img_st_supermarket.png",
       key: 7,
       href: "#",
     },
     {
       name: "فست فود",
-      src: "https://cdn.snappfood.ir/uploads/images/review-app/icons/count/desktop_1_603508bf202d8_img_st_food.png",
+      image:
+        "https://cdn.snappfood.ir/uploads/images/review-app/icons/count/desktop_1_603508bf202d8_img_st_food.png",
       key: 12,
       href: "#",
     },
     {
       name: "نان",
-      src: "https://cdn.snappfood.ir/uploads/images/review-app/icons/count/desktop_5_60350898c61b5_img_st_bakery.png",
+      image:
+        "https://cdn.snappfood.ir/uploads/images/review-app/icons/count/desktop_5_60350898c61b5_img_st_bakery.png",
       key: 13,
       href: "#",
     },
     {
       name: "گوشت",
-      src: "https://cdn.snappfood.ir/uploads/images/review-app/icons/count/desktop_11_603507afc9a32_img_st_meat.png",
+      image:
+        "https://cdn.snappfood.ir/uploads/images/review-app/icons/count/desktop_11_603507afc9a32_img_st_meat.png",
       key: 14,
       href: "#",
     },
     {
       name: "میوه",
-      src: "https://cdn.snappfood.ir/uploads/images/review-app/icons/count/desktop_6_6035088cbcde4_img_st_fruit.png",
+      image:
+        "https://cdn.snappfood.ir/uploads/images/review-app/icons/count/desktop_6_6035088cbcde4_img_st_fruit.png",
       key: 15,
       href: "#",
     },
     {
       name: "آجیل",
-      src: "https://cdn.snappfood.ir/uploads/images/review-app/icons/count/desktop_7_60350883d6e43_img_st_nut.png",
+      image:
+        "https://cdn.snappfood.ir/uploads/images/review-app/icons/count/desktop_7_60350883d6e43_img_st_nut.png",
       key: 16,
       href: "#",
     },
     {
       name: "سوپری",
-      src: "https://cdn.snappfood.ir/uploads/images/review-app/icons/count/desktop_4_603508a14ab73_img_st_supermarket.png",
+      image:
+        "https://cdn.snappfood.ir/uploads/images/review-app/icons/count/desktop_4_603508a14ab73_img_st_supermarket.png",
       key: 17,
       href: "#",
     },
@@ -85,14 +97,23 @@ export const DefaultVendorNav = () => {
 }
 
 export const WithoutImages = () => {
-  const noImages = props.items.map((item) => {
-    item.src = undefined
-    return item
+  const noImages = props.items.map(item => {
+    return { ...item, image: undefined }
   })
 
-  const x :ComponentProps<typeof VendorNav> = {
-    items : noImages
+  const filteredProps: ComponentProps<typeof VendorNav> = {
+    items: noImages,
   }
+  return <VendorNav {...filteredProps} />
+}
 
-  return <VendorNav {...x} />
+export const WithoutBadImages = () => {
+  const noImages = props.items.map(item => {
+    return { ...item, image: "noSuchUrl.com" }
+  })
+
+  const filteredProps: ComponentProps<typeof VendorNav> = {
+    items: noImages,
+  }
+  return <VendorNav {...filteredProps} />
 }
