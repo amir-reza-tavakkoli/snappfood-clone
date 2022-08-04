0c3be7ddb84684707a868c6697a84a491aebf5d9
Amir <72388304+amir-reza-tavakkoli@users.noreply.github.com>
2022-08-04 18:17:55 +0430

    make possible to pass image as direct prop

--- src/components/vendor-nav.tsx
+++ src/components/vendor-nav.tsx
@@ -1,6 +1,12 @@
+import type { ReactNode } from "react"
 import "./vendor-nav.css"
 
-export type item = { name: string; src?: string; href?: string; key: number }
+export type item = {
+  name: string
+  image?: string | ReactNode
+  href?: string
+  key: number
+}
 
 type VendorNavProps = {
   type?: string
@@ -10,16 +16,22 @@ type VendorNavProps = {
 export const VendorNav = ({ items, type }: VendorNavProps) => {
   return (
     <nav className="vendor-nav">
-      {type ? <p className="nonvisual" aria-label="type">
-        {type}
-      </p> : null}
+      {type ? (
+        <p className="nonvisual">
+          {type}
+        </p>
+      ) : null}
       <ul className="_items">
         {items.map((item, index, array) => {
           return (
             <li key={item.key ?? index}>
               <a href={item.href ?? "#"} className="_link">
                 <div>
-                  {item.src ? <img src={item.src} alt={item.name}></img> : null}
+                  {item.image && typeof item.image === "string" ? (
+                    <img src={item.image} alt={item.name}></img>
+                  ) : item.image ? (
+                    item.image
+                  ) : null}
                   <span className="_name">{item.name}</span>
                 </div>
               </a>
