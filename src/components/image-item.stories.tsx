6432853f645e86a31a3f952e2adfbb29e84d0b58
Amir <72388304+amir-reza-tavakkoli@users.noreply.github.com>
2022-08-04 18:48:55 +0430

    add new stories

--- src/components/image-item.stories.tsx
+++ src/components/image-item.stories.tsx
@@ -7,8 +7,41 @@ const imageItemProps: ComponentProps<typeof ImageItem> = {
   title: "ایرانی",
 }
 
-export const imageItemDefault = () => {
+export const ImageItemDefault = () => {
   return <ImageItem {...imageItemProps}></ImageItem>
+}
+
+export const WithoutName = () => {
+  const props = { ...imageItemProps, title: undefined }
+  return <ImageItem {...props}></ImageItem>
+}
+
+export const WithLongName = () => {
+  const props = {
+    ...imageItemProps,
+    title:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  }
+  return <ImageItem {...props}></ImageItem>
+}
+
+export const WithOverflowingName = () => {
+  const props = {
+    ...imageItemProps,
+    title:
+      "LoremIpsumissimplydummytextoftheprintingandtypesettingindustryLoremIpsumhasbeentheindustry'sstandarddummtexteversincethe1500s,whenanunknownprinter ookagalleyoftypeandscrambledittomakeatypejjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj",
+  }
+  return <ImageItem {...props}></ImageItem>
+}
+
+export const WithoutImage = () => {
+  const props = { ...imageItemProps, src: undefined }
+  return <ImageItem {...props}></ImageItem>
+}
+
+export const WithBadImage = () => {
+  const props = { ...imageItemProps, src: "noSuchUrl.com" }
+  return <ImageItem {...props}></ImageItem>
 }
 
 export const Constrained = () => {
