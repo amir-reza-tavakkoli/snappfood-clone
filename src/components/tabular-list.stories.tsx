e1eaea8b6e20ddfcde8a48c1cba022ef24d1b537
Amir <72388304+amir-reza-tavakkoli@users.noreply.github.com>
2022-08-05 11:19:45 +0430

    add componentprops and add real world data

--- src/components/tabular-list.stories.tsx
+++ src/components/tabular-list.stories.tsx
@@ -1,43 +1,37 @@
+import type { ComponentProps } from "react";
 import { TabularList } from "./tabular-list";
 
-const y = {
+const props : ComponentProps<typeof TabularList> = {
   title: "اسنپ‌فود در شهرهای ایران",
   type: "cities",
   items: [
     { name: "شیراز", href: "#" },
+    { name: "تهران", href: "#" },
+    { name: "بندر عباس", href: "#" },
+    { name: "شهرکرد", href: "#" },
+    { name: "آباده", href: "#" },
+    { name: "قزوین", href: "#" },
+    { name: "ساری", href: "#" },
+    { name: "کرمان", href: "#" },
+    { name: "عباس شهر", href: "#" },
+    { name: "مشهد", href: "#" },
+    { name: "بیرجند", href: "#" },
     { name: "شیراز", href: "#" },
-    { name: "شیراز", href: "#" },
-    { name: "شیراز", href: "#" },
-    { name: "شیراز", href: "#" },
-    { name: "شیراز", href: "#" },
-    { name: "شیراز", href: "#" },
-    { name: "شیراز", href: "#" },
-    { name: "شیراز", href: "#" },
-    { name: "شیراز", href: "#" },
-    { name: "شیراز", href: "#" },
-    { name: "شیراز", href: "#" },
-    { name: "شیراز", href: "#" },
-    { name: "شیراز", href: "#" },
-    { name: "شیراز", href: "#" },
-    { name: "شیراز", href: "#" },
-    { name: "شیراز", href: "#" },
-    { name: "شیراز", href: "#" },
-    { name: "شیراز", href: "#" },
-    { name: "شیراز", href: "#" },
-    { name: "شیراز", href: "#" },
-    { name: "شیراز", href: "#" },
-    { name: "شیراز", href: "#" },
-    { name: "شیراز", href: "#" },
-    { name: "شیراز", href: "#" },
-    { name: "شیراز", href: "#" },
-    { name: "شیراز", href: "#" },
-    { name: "شیراز", href: "#" },
-    { name: "شیراز", href: "#" },
+    { name: "تهران", href: "#" },
+    { name: "بندر عباس", href: "#" },
+    { name: "شهرکرد", href: "#" },
+    { name: "آباده", href: "#" },
+    { name: "قزوین", href: "#" },
+    { name: "ساری", href: "#" },
+    { name: "کرمان", href: "#" },
+    { name: "عباس شهر", href: "#" },
+    { name: "مشهد", href: "#" },
+    { name: "بیرجند", href: "#" },
   ],
 }
 
-export const tabularList = () => {
+export const TabularListDefault = () => {
     return (
-        <TabularList {...y}></TabularList>
+        <TabularList {...props}></TabularList>
     )
 }
