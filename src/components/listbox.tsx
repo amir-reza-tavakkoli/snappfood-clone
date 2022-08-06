01177be6a687ed15984ee713a9f8d90ce11301c6
Amir <72388304+amir-reza-tavakkoli@users.noreply.github.com>
2022-08-06 16:51:46 +0430

    add listbox markup

---
+++ src/components/listbox.tsx
@@ -0,0 +1,41 @@
+import "./listbox.css"
+
+type ListBoxProps = {
+  title: string
+  items: {
+    name: string
+    img: string
+    key?: number
+    selected?: boolean
+  }[]
+}
+
+export const ListBox = ({ title, items }: ListBoxProps) => {
+  let selected: string | undefined
+  const listItems = items.map((item, index, array) => {
+    const id = `ListBox + ${item.name} + ${index}`
+    return (
+      <li
+        role="option"
+        key={item.key ?? index}
+        aria-selected={
+          item.selected ? ((selected = id), item.selected) : item.selected
+        }
+        id={id}
+      >
+        <img src={item.name} alt="" role="presentation" />
+        {item.name}
+      </li>
+    )
+  })
+  return (
+    <ul
+      tabIndex={0}
+      role="listbox"
+      aria-label={title}
+      aria-activedescendant={selected}
+    >
+      {listItems}
+    </ul>
+  )
+}
