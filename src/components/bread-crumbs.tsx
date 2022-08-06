c44241e5ee366b611dc57f8e6099cded1040fe11
Amir <72388304+amir-reza-tavakkoli@users.noreply.github.com>
2022-08-06 12:15:31 +0430

    add new component markup

---
+++ src/components/bread-crumbs.tsx
@@ -0,0 +1,45 @@
+import { useMemo } from "react"
+import type { ReactNode } from "react"
+import "./bread-crumbs.css"
+import { LeftGrayFlash } from "./svg"
+
+type Item = {
+  name: string
+  href: string
+  key: number
+  successor?: Item
+}
+
+type BreadCrumbsProps = {
+  title: string
+  item?: Item
+}
+
+export const BreadCrumbs = ({ title, item }: BreadCrumbsProps) => {
+  const memoizedItems = useMemo(() => {
+    const items: ReactNode[] = []
+    while (item) {
+      items.push(
+        <li key={item.key}>
+          <a href={item.href} aria-current={item && item.successor ? undefined : "page"}>
+            {" "}
+            {item.name}{" "}
+          </a>
+          {item.successor ? (
+            <LeftGrayFlash role="presentation"></LeftGrayFlash>
+          ) : null}
+        </li>,
+      )
+      if (item) {
+        item = item.successor
+      }
+    }
+    return items
+  }, [item])
+
+  return (
+    <nav aria-label="Breadcrumb" className="bread-crumbs">
+      <ol aria-label={title}>{memoizedItems}</ol>
+    </nav>
+  )
+}
