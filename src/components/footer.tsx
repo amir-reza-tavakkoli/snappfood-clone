7a62aa02e51810c0faf3103125dc48f19dacee92
Amir <72388304+amir-reza-tavakkoli@users.noreply.github.com>
2022-08-07 11:44:07 +0430

    add markup for footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,91 @@
+import type { ReactNode } from "react"
+import "./footer.css"
+
+type Refrence = { name: string; href: string; logo: string; key?: number }
+
+type FooterProps = {
+  name: string
+  logo: string
+  homepage: string
+  description?: string
+  links?: { description?: string; items: Refrence[] }
+  contacts?: { description?: string; items: Refrence[] }
+  credentials?: { description?: string; items: Refrence[] }
+}
+
+export const Footer = ({
+  logo,
+  name,
+  homepage,
+  description,
+  links,
+  credentials,
+  contacts,
+}: FooterProps) => {
+  return (
+    <footer className="footer">
+      <address>
+        <div className="address">
+          <a href={homepage} aria-label="Homepage" rel="canonical">
+            <img src={logo} role="presentation" alt="" />
+          </a>
+          {name}
+          <p>{description}</p>
+        </div>
+        {contacts ? (
+          <ul
+            aria-label={contacts.description ?? undefined}
+            className="contacts"
+          >
+            {contacts.items.map((item, index, array) => {
+              return (
+                <li key={item.key ?? index}>
+                  <a href={item.href} rel="external" aria-label={item.name}>
+                    <img src={item.logo} role="presentation" alt="" />
+                  </a>
+                </li>
+              )
+            })}
+          </ul>
+        ) : null}
+      </address>
+
+      {links ? (
+        <nav>
+          <ul aria-label={links.description ?? undefined} className="links">
+            {links.items.map((item, index, array) => {
+              return (
+                <li key={item.key ?? index}>
+                  <a href={item.href}>{item.name}</a>
+                </li>
+              )
+            })}
+          </ul>
+        </nav>
+      ) : null}
+
+      {credentials ? (
+        <address>
+          <ul
+            className="credentials"
+            aria-label={credentials.description ?? undefined}
+          >
+            {credentials.items.map((item, index, array) => {
+              return (
+                <li key={item.key ?? index}>
+                  <a
+                    href={item.href}
+                    rel="external license"
+                    aria-label={item.name}
+                  >
+                    <img src={item.logo} role="presentation" alt="" />
+                  </a>
+                </li>
+              )
+            })}
+          </ul>
+        </address>
+      ) : null}
+    </footer>
+  )
+}
