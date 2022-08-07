7b7c512a2ad896c349003823f4f5bf5c626c407b
Amir <72388304+amir-reza-tavakkoli@users.noreply.github.com>
2022-08-07 13:29:13 +0430

    add empty story and css file

---
+++ src/components/footer.stories.tsx
@@ -0,0 +1,8 @@
+import { ComponentProps } from "react";
+import { Footer } from "./footer";
+
+const props: ComponentProps<typeof Footer> = {
+  name: "SnappFood",
+  description: "Experince the delightful sex",
+  homepage: "snappfood.ir",
+}
