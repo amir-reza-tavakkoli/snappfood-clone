c1204399ac33d9453682e288a9e83674d02cbfaf
Amir <72388304+amir-reza-tavakkoli@users.noreply.github.com>
2022-08-08 14:50:22 +0430

    add new icon component

---
+++ src/components/icon.stories.tsx
@@ -0,0 +1,11 @@
+import { Icon } from "./icon"
+
+export const DefaultSnappfood = () => <Icon name="snappfood" color="accent" />
+
+export const BlackSnappfood = () => <Icon name="snappfood" color="text" />
+
+export const DefaultBaazr = () => <Icon name="bazzar" />
+
+export const GreenUser = () => <Icon name="user" color="action"></Icon>
+
+export const RedFlash = () => <Icon name="flash" color="error"></Icon>
