7125291175ad730b0a958edad6cb5dcc43ff2cc9
Amir <72388304+amir-reza-tavakkoli@users.noreply.github.com>
2022-08-09 23:37:25 +0430

    fix issues in story

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,16 +1,23 @@
-import type { ReactNode } from "react"
+import { ComponentProps } from "react"
 import "./footer.css"
+import { Icon } from "./icon"
 
-type Reference = { name: string; href: string; logo: string }
+type Reference = {
+  name?: string
+  href: string
+}
+
+type WithImage = Reference & { logo: string }
+type WithIcon = Reference & { logo: ComponentProps<typeof Icon> }
 
 type FooterProps = {
   name: string
-  logo: string
+  logo: ComponentProps<typeof Icon>
   homepage: string
   description?: string
-  links: { description?: string; items: Reference[] }
-  contacts: { description?: string; items: Reference[] }
-  credentials: { description?: string; items: Reference[] }
+  links: Reference[]
+  contacts: WithIcon[]
+  credentials: WithImage[]
 }
 
 export const Footer = ({
@@ -24,40 +31,42 @@ export const Footer = ({
 }: FooterProps) => {
   return (
     <footer className="footer">
-      <p className="_vendor">
-        <a href={homepage} aria-label="Homepage" rel="canonical">
-          <img src={logo} role="presentation" alt="" />
-        </a>
-        {name}
-        <span>{description}</span>
-      </p>
+      <div>
+        <p className="_vendor">
+          <a href={homepage} aria-label="Homepage" rel="canonical">
+            <Icon name={logo.name} role="presentation" />
+          </a>
+          <span>{name}</span>
+          <span>{description}</span>
+        </p>
 
-      <address
-        aria-label={contacts.description ?? undefined}
-        className="_contacts"
-      >
-        {contacts.items.map((item, index) => (
-          <li key={index}>
-            <a href={item.href} rel="external" aria-label={item.name}>
-              <img src={item.logo} role="presentation" alt="" />
-            </a>
-          </li>
-        ))}
-      </address>
+        <address aria-label="Contacts">
+          <ul className="_contacts">
+            {contacts.map((item, index) => (
+              <li key={index}>
+                <a href={item.href} rel="external" aria-label={item.logo.name}>
+                  <Icon
+                    name={item.logo.name}
+                    color="faded"
+                    role="presentation"
+                  />
+                </a>
+              </li>
+            ))}
+          </ul>
+        </address>
+      </div>
 
-      <ul aria-label={links.description ?? undefined} className="_links">
-        {links.items.map((item, index) => (
+      <ul aria-label="Sitemap" className="_sitemap">
+        {links.map((item, index) => (
           <li key={index}>
             <a href={item.href}>{item.name}</a>
           </li>
         ))}
       </ul>
 
-      <ul
-        className="_credentials"
-        aria-label={credentials.description ?? undefined}
-      >
-        {credentials.items.map((item, index) => (
+      <ul className="_credentials" aria-label="Credentials">
+        {credentials.map((item, index) => (
           <li key={index}>
             <a href={item.href} rel="external license" aria-label={item.name}>
               <img src={item.logo} role="presentation" alt="" />
