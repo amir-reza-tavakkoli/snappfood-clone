21a3c7a4513e970948efd236da4abd31e748faec
Amir <72388304+amir-reza-tavakkoli@users.noreply.github.com>
2022-08-09 14:07:08 +0430

    simplify markup

--- src/components/bread-crumbs.stories.tsx
+++ src/components/bread-crumbs.stories.tsx
@@ -2,43 +2,35 @@ import type { ComponentProps } from "react"
 import { BreadCrumbs } from "./bread-crumbs"
 
 const props: ComponentProps<typeof BreadCrumbs> = {
-  title: "links",
   items: [
     {
       name: "Snappfood",
       href: "#",
-      key: 1,
     },
     {
       name: "Bakeries",
       href: "#",
-      key: 2,
     },
     {
       name: "Shiraz bakeries",
       href: "#",
-      key: 3,
     },
   ],
 }
 
 const rtlProps: ComponentProps<typeof BreadCrumbs> = {
-  title: "لینک ها",
   items: [
     {
       name: "اسنپ فود",
       href: "#",
-      key: 1,
     },
     {
       name: " نانوایی ها",
       href: "#",
-      key: 2,
     },
     {
       name: "نانوایی های شیراز",
       href: "#",
-      key: 3,
     },
   ],
 }
@@ -49,8 +41,8 @@ export const BreadCrumbsDefault = () => {
 
 export const DefaultRTL = () => {
   return (
-    <html dir="rtl">
+    <div dir="rtl">
       <BreadCrumbs {...rtlProps}></BreadCrumbs>
-    </html>
+    </div>
   )
 }
--- src/components/bread-crumbs.tsx
+++ src/components/bread-crumbs.tsx
@@ -4,27 +4,25 @@ import { Icon } from "./icon"
 type Item = {
   name: string
   href: string
-  key?: number
 }
 
 type BreadCrumbsProps = {
-  title: string
   items: Item[]
 }
 
-export const BreadCrumbs = ({ title, items }: BreadCrumbsProps) => {
+export const BreadCrumbs = ({ items }: BreadCrumbsProps) => {
   return (
     <nav aria-label="Breadcrumb" className="bread-crumbs">
-      <ol aria-label={title}>
-        {items.map((item, index, array) => (
-          <li key={item.key ?? index}>
+      <ol>
+        {items.map((item, index) => (
+          <li key={index}>
             <a
               href={item.href}
-              aria-current={index === array.length - 1 ? "page" : undefined}
+              aria-current={index === items.length - 1 ? "page" : undefined}
             >
               {item.name}
             </a>
-            {index !== array.length - 1 ? (
+            {index !== items.length - 1 ? (
               <Icon name="flash" color="faded" role="presentation" />
             ) : null}
           </li>
