30d353f41f15d88a4108a1e67411fafd82d4b776
Amir <72388304+amir-reza-tavakkoli@users.noreply.github.com>
2022-08-13 22:29:05 +0430

    feat: implement food card component (#20)
    * add basic component structure and style
    
    * fix issues in markup and style and add more stories
    
    * add new color
    
    * change structure in component
    
    * change nested div to dl for better a11y
    
    * fix issues in story

---
+++ src/components/food-card.stories.tsx
@@ -0,0 +1,108 @@
+import type { ComponentProps } from "react"
+import { FoodCard } from "./food-card"
+
+const props: ComponentProps<typeof FoodCard> = {
+  name: "پیتزا استیک",
+  type: "غذا",
+  ingredients: ["گوشت خوک مرینت شده", "پنیر گودا", "سس ناپولی", "ریحان"],
+  available: true,
+  image:
+    "https://cdn.snappfood.ir/641x641/cdn/71/82/3/vendor/62b604a15b978.jpeg",
+  prices: [
+    {
+      variation: "بزرگ",
+      vaule: 70000,
+      currency: "تومان",
+      available: true,
+    },
+    {
+      variation: "کوچک",
+      vaule: 55000,
+      currency: "تومان",
+      available: true,
+    },
+  ],
+}
+
+export const FoodCardDefault = () => {
+  return <FoodCard {...props}></FoodCard>
+}
+
+export const LongIngerdients = () => {
+  const longIngredients: ComponentProps<typeof FoodCard> = {
+    ...props,
+    ingredients: [
+      "گوشت خوک مرینت شده",
+      "پنیر گودا",
+      "سس ناپولی",
+      "ریحان",
+      "گوشت خوک مرینت شده",
+      "پنیر گودا",
+      "سس ناپولی",
+      "ریحان",
+      "گوشت خوک مرینت شده",
+      "پنیر گودا",
+      "سس ناپولی",
+      "ریحان",
+      "گوشت خوک مرینت شده",
+      "پنیر گودا",
+      "سس ناپولی",
+      "ریحان",
+    ],
+  }
+  return <FoodCard {...longIngredients}></FoodCard>
+}
+
+export const LongIngerdientsConstrained = () => {
+  const longIngredients: ComponentProps<typeof FoodCard> = {
+    ...props,
+    ingredients: [
+      "گوشت خوک مرینت شده",
+      "پنیر گودا",
+      "سس ناپولی",
+      "ریحان",
+      "گوشت خوک مرینت شده",
+      "پنیر گودا",
+      "سس ناپولی",
+      "ریحان",
+      "گوشت خوک مرینت شده",
+      "پنیر گودا",
+      "سس ناپولی",
+      "ریحان",
+      "گوشت خوک مرینت شده",
+      "پنیر گودا",
+      "سس ناپولی",
+      "ریحان",
+    ],
+  }
+  return (
+    <div style={{ width: "300px", height: "250px" }}>
+      {" "}
+      <FoodCard {...longIngredients}></FoodCard>
+    </div>
+  )
+}
+
+export const BadImage = () => {
+  const longIngredients: ComponentProps<typeof FoodCard> = {
+    ...props,
+    image: "NoSuchUrl.com",
+  }
+  return <FoodCard {...longIngredients}></FoodCard>
+}
+
+export const SmallConstrained = () => {
+  return (
+    <div style={{ width: "300px", height: "250px" }}>
+      <FoodCard {...props}></FoodCard>
+    </div>
+  )
+}
+
+export const MediumConstrained = () => {
+  return (
+    <div style={{ width: "500px", height: "40px" }}>
+      <FoodCard {...props}></FoodCard>
+    </div>
+  )
+}
---
+++ src/components/food-card.tsx
@@ -0,0 +1,100 @@
+import { Button } from "./button"
+import "./food-card.css"
+
+type FoodCardProps = {
+  name: string
+  type: string
+  ingredients?: string[]
+  available: boolean
+  image?: string
+  prices?: {
+    variation?: string
+    vaule: number
+    currency: string
+    available: boolean
+  }[]
+}
+
+export const FoodCard = ({
+  name,
+  type,
+  image,
+  ingredients,
+  prices,
+  available,
+}: FoodCardProps) => {
+  return (
+    <dl className="food-card">
+      <div>
+        <dt className="nonvisual">Item</dt>
+        <dl className="_identity">
+          <dt className="nonvisual">Name</dt>
+          <dd className="_name">{name}</dd>
+          <dt className="nonvisual">Type</dt>
+          <dd className="nonvisual">{type}</dd>
+          {ingredients ? (
+            <>
+              <dt className="nonvisual">Ingredients</dt>
+              <dd className="_ingredients">
+                <ul>
+                  {ingredients.map((item, index) => (
+                    <li key={index}>
+                      {item} {index !== ingredients.length - 1 ? ", " : " "}
+                    </li>
+                  ))}
+                </ul>
+              </dd>
+            </>
+          ) : null}
+        </dl>
+        <img
+          src={image ?? "https://snappfood.ir/static/images/placeholder.png"}
+          alt=""
+          role="presentation"
+        />
+      </div>
+      {prices!.map((item, index) => (
+        <div>
+          {item ? (
+            <>
+              <dt className="nonvisual">Price</dt>
+              <dd>
+                <dl className="_price">
+                  <dt className="nonvisual">Variation</dt>
+                  <dd className="_variation">{item.variation}</dd>
+                  <div>
+                    <dt className="nonvisual">Value</dt>
+                    <dd>{item.vaule}</dd>
+                    <dt className="nonvisual">Currency</dt>
+                    <dd>{item.currency}</dd>
+                  </div>
+                </dl>
+              </dd>
+            </>
+          ) : null}
+          <dt className="nonvisual">Add</dt>
+          <dd>
+            {available && item.available ? (
+              <Button
+                variant="primary"
+                rounding="full"
+                aria-label={"Add" + name + item.variation}
+              >
+                افزودن
+              </Button>
+            ) : (
+              <Button
+                disabled
+                variant="primary"
+                rounding="full"
+                aria-label="Not available"
+              >
+                افزودن
+              </Button>
+            )}
+          </dd>
+        </div>
+      ))}
+    </dl>
+  )
+}
