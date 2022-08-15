f4ee41113fdc248ffea6336f0e46ea7973fbc200
Amir <72388304+amir-reza-tavakkoli@users.noreply.github.com>
2022-08-15 11:33:14 +0430

    fix: add new component to icons and append it to store card (#22)

--- src/components/store-card.tsx
+++ src/components/store-card.tsx
@@ -78,7 +78,7 @@ export const StoreCard = ({ name, logo, type, rating }: StoreCardProps) => {
       <div>
         <dt className="nonvisual">Information</dt>
         <dd>
-          <Button variant="primary" type="button" rounding="full">
+          <Button variant="primary" type="button" rounding="full" icon={{name : "info", color : "action"}}>
             اطلاعات و نظرات
           </Button>
         </dd>
