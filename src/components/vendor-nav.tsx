3800cde7b28a60a142d241b697b03649873bbf48
Amir <72388304+amir-reza-tavakkoli@users.noreply.github.com>
2022-08-30 18:03:16 +0430

    fix markup excessive classnames

--- src/components/vendor-nav.tsx
+++ src/components/vendor-nav.tsx
@@ -18,22 +18,20 @@ export const VendorNav = ({ items, type }: VendorNavProps) => {
     <nav className="vendor-nav">
       {type ? <p className="nonvisual">{type}</p> : null}
       <ul className="_items">
-        {items.map((item, index, array) => {
-          return (
-            <li key={item.key ?? index}>
-              <a href={item.href ?? "#"} className="_link">
-                <div>
-                  {item.image && typeof item.image === "string" ? (
-                    <img src={item.image} alt=""></img>
-                  ) : item.image ? (
-                    item.image
-                  ) : null}
-                  <span className="_name">{item.name}</span>
-                </div>
-              </a>
-            </li>
-          )
-        })}
+        {items.map((item, index) => (
+          <li key={item.key ?? index}>
+            <a href={item.href ?? "#"} className="_link">
+              <div>
+                {item.image && typeof item.image === "string" ? (
+                  <img src={item.image} alt=""></img>
+                ) : item.image ? (
+                  item.image
+                ) : null}
+                <span className="_name">{item.name}</span>
+              </div>
+            </a>
+          </li>
+        ))}
       </ul>
     </nav>
   )
